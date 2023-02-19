bff05384796c38c3fbf940baa344d6f3bda0696d
devesh <deveshm.cs.20@nitj.ac.in>
2023-02-19 07:36:06 +0530

    Change: Moved Main.js to src

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
-import { useFonts, Lato_400Regular, Lato_900Black, Lato_700Bold } from '@expo-google-fonts/lato';
-import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
-import Main from "./Main";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Main from './src';
+import { StatusBar } from 'react-native';
 
 export default function App() {
-  let [fontsLoaded] = useFonts({
-    Lato_400Regular, Lato_900Black, Lato_700Bold
-  });
 
-  if (!fontsLoaded) {
-    return null;
-  };
   return (
     <SafeAreaProvider>
-          <Main />
+      <StatusBar
+        animated={true}
+        backgroundColor="#7345F6"
+        barStyle={'light-content'}
+        showHideTransition={'fade'}
+        hidden={false}
+      />
+      <Main />
     </SafeAreaProvider>
   );
 }
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useFonts, Lato_400Regular, Lato_900Black, Lato_700Bold } from '@expo-google-fonts/lato';
+
+import TabNavigator from "./navigation";
+
+
+export default function Main() {
+    let [fontsLoaded] = useFonts({
+        Lato_400Regular, Lato_900Black, Lato_700Bold
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    };
+    const insets = useSafeAreaInsets();
+
+    return (
+        <View style={{ marginTop: insets.top, flex: 1 }}>
+            
+            <NavigationContainer>
+                <TabNavigator/>
+            </NavigationContainer>
+        </View>
+    );
+}
