696e9c700b9742d4067539bd285db6150aee8849
devesh <deveshm.cs.20@nitj.ac.in>
2023-02-19 07:27:27 +0530

    Add: Added Main.js, Fixed flickering due to stacking navigation with top-tabs, Added SafeAreaProvider in App.js & insets fix in Main.js

--- App.js
+++ App.js
@@ -1,26 +1,18 @@
-import { SafeAreaView, StatusBar ***REMOVED*** from "react-native";
-import Navigator from "./src/navigation";
-import { useFonts, Lato_400Regular, Lato_900Black ***REMOVED*** from '@expo-google-fonts/lato';
+import { useFonts, Lato_400Regular, Lato_900Black, Lato_700Bold ***REMOVED*** from '@expo-google-fonts/lato';
+import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets ***REMOVED*** from 'react-native-safe-area-context';
+import Main from "./Main";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    Lato_400Regular, Lato_900Black
+    Lato_400Regular, Lato_900Black, Lato_700Bold
   ***REMOVED***
 
   if (!fontsLoaded) {
     return null;
 ***REMOVED***;
-
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <StatusBar
-        animated={true***REMOVED***
-        backgroundColor="#7345F6"
-        barStyle={'light-content'***REMOVED***
-        showHideTransition={'fade'***REMOVED***
-        hidden={false***REMOVED***
-      />
-      <Navigator />
-    </SafeAreaView>
+    <SafeAreaProvider>
+          <Main />
+    </SafeAreaProvider>
   );
 ***REMOVED***
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,20 +1,153 @@
-import Home from '../screens';
-import { NavigationContainer ***REMOVED*** from '@react-navigation/native';
 import React from 'react'
-import TabNavigator from './TabNavigator';
+import { Feather ***REMOVED*** from '@expo/vector-icons';
+import { Dimensions, StyleSheet ***REMOVED*** from 'react-native';
 import { createNativeStackNavigator ***REMOVED*** from '@react-navigation/native-stack';
+import { createMaterialTopTabNavigator ***REMOVED*** from '@react-navigation/material-top-tabs';
 
-const Stack = createNativeStackNavigator();
+import Home from '../screens';
+import Settings from '../screens/settings';
+import Cafeteria from '../screens/cafeteria';
+import Appointments from '../screens/appointments';
 
-const Navigator = () => {
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="main" component={TabNavigator***REMOVED*** options={{ headerShown: false ***REMOVED******REMOVED*** />
-                <Stack.Screen name="Home" component={Home***REMOVED*** />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <HomeStack.Navigator screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: {
+                
+                borderBottomWidth: StyleSheet.hairlineWidth,
+        ***REMOVED***,
+            headerTitleStyle: {
+                fontSize: 15,
+                fontFamily: 'Lato_700Bold',
+        ***REMOVED***
+    ***REMOVED******REMOVED***>
+            <HomeStack.Screen name="Home" component={Home***REMOVED*** />
+        </HomeStack.Navigator>
+    );
+***REMOVED***
+
+const AppointmentsStack = createNativeStackNavigator();
+
+function AppointmentsStackScreen() {
+    return (
+        <AppointmentsStack.Navigator screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: {
+                
+                borderBottomWidth: StyleSheet.hairlineWidth,
+        ***REMOVED***,
+            headerTitleStyle: {
+                fontSize: 15,
+                fontFamily: 'Lato_700Bold',
+        ***REMOVED***
+    ***REMOVED******REMOVED***>
+            <AppointmentsStack.Screen name="Appointments" component={Appointments***REMOVED*** />
+        </AppointmentsStack.Navigator>
     )
 ***REMOVED***
 
-export default Navigator;
+const CafeteriaStack = createNativeStackNavigator();
+
+function CafeteriaStackScreen() {
+    return (
+        <CafeteriaStack.Navigator screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: {
+                
+                borderBottomWidth: StyleSheet.hairlineWidth,
+        ***REMOVED***,
+            headerTitleStyle: {
+                fontSize: 15,
+                fontFamily: 'Lato_700Bold',
+        ***REMOVED***
+    ***REMOVED******REMOVED***>
+            <CafeteriaStack.Screen name="Cafeteria" component={Cafeteria***REMOVED*** />
+        </CafeteriaStack.Navigator>
+    )
+***REMOVED***
+
+const SettingsStack = createNativeStackNavigator();
+
+function SettingsStackScreen() {
+    return (
+        <SettingsStack.Navigator screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: {
+                
+                borderBottomWidth: StyleSheet.hairlineWidth,
+        ***REMOVED***,
+            headerTitleStyle: {
+                fontSize: 15,
+                fontFamily: 'Lato_700Bold',
+        ***REMOVED***
+    ***REMOVED******REMOVED***>
+            <SettingsStack.Screen name="Settings" component={Settings***REMOVED*** />
+        </SettingsStack.Navigator>
+    )
+***REMOVED***
+
+const Tab = createMaterialTopTabNavigator();
+
+const TabNavigator = () => {
+
+    return (
+
+        <Tab.Navigator tabBarPosition='bottom' screenOptions={{
+            tabBarActiveTintColor: "#7345F6",
+            tabBarInactiveTintColor: "gray",
+            tabBarPressColor: 'transparent',
+            tabBarPressOpacity: 0,
+            tabBarIndicatorStyle: {
+                width: 21,
+                backgroundColor: '#7345F6',
+                left: (Dimensions.get('window').width / 4 - 19) / 2,
+                marginBottom: 9,
+                height: 4,
+                borderRadius: 100,
+        ***REMOVED***,
+            tabBarStyle: [
+                {
+                    height: 55,
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,
+                    shadowOpacity: 0.58,
+                    shadowRadius: 16.0,
+                    elevation: 24,
+            ***REMOVED***,
+                null
+            ],
+    ***REMOVED******REMOVED*** initialRouteName="home" keyboardDismissMode='on-drag'>
+            <Tab.Screen name="home" component={HomeStackScreen***REMOVED*** options={{
+                title: "",
+                tabBarIcon: ({ color, size ***REMOVED***) => (
+                    <Feather name="home" size={24***REMOVED*** color={color***REMOVED*** />
+                ),
+        ***REMOVED******REMOVED*** />
+            <Tab.Screen name="appointments" component={AppointmentsStackScreen***REMOVED*** options={{
+                title: "",
+                tabBarIcon: ({ color, size ***REMOVED***) => (
+                    <Feather name="calendar" size={24***REMOVED*** color={color***REMOVED*** />
+                ),
+        ***REMOVED******REMOVED*** />
+            <Tab.Screen name="cafeteria" component={CafeteriaStackScreen***REMOVED*** options={{
+                title: "",
+                tabBarIcon: ({ color, size ***REMOVED***) => (
+                    <Feather name="shopping-bag" size={24***REMOVED*** color={color***REMOVED*** />
+                ),
+        ***REMOVED******REMOVED*** />
+            <Tab.Screen name="settings" component={SettingsStackScreen***REMOVED*** options={{
+                title: "",
+                tabBarIcon: ({ color, size ***REMOVED***) => (
+                    <Feather name="settings" size={24***REMOVED*** color={color***REMOVED*** />
+                ),
+        ***REMOVED******REMOVED*** />
+        </Tab.Navigator>
+    )
+***REMOVED***
+
+
+
+export default TabNavigator
