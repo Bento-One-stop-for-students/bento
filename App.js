2f9702ccd08b31156f5ad66d100448958908d8a2
devesh <deveshm.cs.20@nitj.ac.in>
2023-02-19 07:27:27 +0530

    Add: Added Main.js, Fixed flickering due to stacking navigation with top-tabs, Added SafeAreaProvider in App.js & insets fix in Main.js

--- App.js
+++ App.js
@@ -1,26 +1,18 @@
-import { SafeAreaView, StatusBar } from "react-native";
-import Navigator from "./src/navigation";
-import { useFonts, Lato_400Regular, Lato_900Black } from '@expo-google-fonts/lato';
+import { useFonts, Lato_400Regular, Lato_900Black, Lato_700Bold } from '@expo-google-fonts/lato';
+import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
+import Main from "./Main";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    Lato_400Regular, Lato_900Black
+    Lato_400Regular, Lato_900Black, Lato_700Bold
   });
 
   if (!fontsLoaded) {
     return null;
   };
-
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <StatusBar
-        animated={true}
-        backgroundColor="#7345F6"
-        barStyle={'light-content'}
-        showHideTransition={'fade'}
-        hidden={false}
-      />
-      <Navigator />
-    </SafeAreaView>
+    <SafeAreaProvider>
+          <Main />
+    </SafeAreaProvider>
   );
 }
