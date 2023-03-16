3aae1da40f7c8966bccd3e0abda7b507b378b40d
devesh <deveshm.cs.20@nitj.ac.in>
2023-03-16 15:31:11 +0530

    added custom react native code for react-native-firebase

--- AppExtended.js
+++ AppExtended.js
@@ -16,10 +16,81 @@ import Navigator from "./navigation/Navigator";
 import { AuthContext } from "./lib/context/authContext";
 import { handleIsSignedIn } from "./lib/auth";
 import OverLayNotificationModal from "./components/OverLayNotificationModal";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 
 SplashScreen.preventAutoHideAsync();
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
 
 export default function AppExtended({ navigation }) {
+  const [expoPushToken, setExpoPushToken] = React.useState("");
+  const [notification, setNotification] = React.useState(false);
+  const notificationListener = React.useRef();
+  const responseListener = React.useRef();
+
+  console.log("outside");
+  React.useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      console.log(token);
+      setExpoPushToken(token);
+    });
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+    console.log(expoPushToken);
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   const [appIsReady, setAppIsReady] = React.useState(false);
   const { authDispatch } = React.useContext(AuthContext);
   React.useEffect(() => {
@@ -36,7 +107,6 @@ export default function AppExtended({ navigation }) {
         setAppIsReady(true);
       }
     }
-
     prepare();
   }, []);
   const onLayoutRootView = React.useCallback(async () => {
